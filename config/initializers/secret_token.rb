273a93fffb9e0d77d92f6a89c35f1f5987d9095a
GtK <GilliTheKid@AirGtK.local>
2011-06-19 15:53:39 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ranksimple::Application.config.secret_token = '3204a1ebabb3f65c67f29d5217ecfa6da9df74ac19c21ebb91eafa9c96a59531a76b88daa584f1959afacd6e55187b85cac7f32cd327896704f86adc0169b43d'
