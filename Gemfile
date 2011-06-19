24d7c4655efdbd98b081034923b3d672b5604551
GtK <GilliTheKid@AirGtK.local>
2011-06-19 16:52:21 +0200

    First push - Devise member

--- Gemfile
+++ Gemfile
@@ -1,11 +1,24 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.7'
+gem 'pg'
+gem 'simple_form'
+gem 'devise'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'pg'
+group :development do
+	gem 'rspec-rails', '2.0.1'
+	gem 'annotate-models', '1.0.4'
+end
+
+group :test do
+	gem 'rspec', '2.0.1'
+	gem 'webrat', '0.7.1'
+	gem 'spork', '0.8.4'
+	gem 'factory_girl_rails', '1.0'
+end
 
 # Use unicorn as the web server
 # gem 'unicorn'
---
+++ config/initializers/mail_setup.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "upstream.info",
+  :user_name            => "308sunningdale@gmail.com",
+  :password             => "snelock232",
+  :authentication       => "plain",
+# changed this from true because of a seg fault (pg only?) - http://redmine.ruby-lang.org/issues/4413
+# set to true for Heroku deploy? -- not sure why?
+  :enable_starttls_auto => true 
+}
+
